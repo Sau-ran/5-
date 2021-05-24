a2d3dc506690d715a95ed5dce571953d2c1e84f2
Sau-ran <57310009+Sau-ran@users.noreply.github.com>
2021-05-24 14:14:09 +0500

    Add files via upload

---
+++ задание 4/prog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+char str[128];
+float b;
+  while (scanf("%s" , str))
+	{
+
+	if (!strcmp(str, "\"temperature\":"))
+		{
+		scanf("%f", &b);
+		printf("%f \n",b);
+		}
+	if (b<0)
+	{
+	}
+
+		else
+		{
+			if (b>= 10,0)
+			{
+		system ("mosquitto_pub -h  \"10.96.4.11\" -t \"devices/lora/807B85902000069B/gpio\" -m \"set 1 1\" -q 1");
+			}
+			else
+			{
+		system ("mosquitto_pub -h  \"10.96.4.11\" -t \"devices/lora/807B85902000069B/gpio\" -m \"set 1 0\" -q 1");
+			}
+		}
+
+	}
+  return 0;
+}
